217973595339d48fa46ba09402e836aca77e3334
Ashlyn Joshy <ashlyntherese@gmail.com>
2024-08-20 19:44:05 +0530

    tailwind setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-     <h1>Welcome to the Blog Application</h1>
+      <h1 className="text-5xl font-bold">Welcome to the Blog Application</h1>
     </div>
   );
 }
