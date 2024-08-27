17c94e8a7cddff2160cfae34f75dc629e70ac5c3
Ashlyn Joshy <ashlyntherese@gmail.com>
2024-08-27 15:08:24 +0530

    user register jwt-token

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.JWT_secret, { expiresIn: "3d" });
+};
 
 module.exports.userLogin = async (req, res) => {
   res.status(200).json({ msg: "login page" });
@@ -8,7 +13,8 @@ module.exports.userRegister = async (req, res) => {
   const { name, email, password } = req.body;
   try {
     const user = await User.Register(name, email, password);
-    res.status(200).json({ user });
+    const token = createToken(user._id);
+    res.status(200).json({ user, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
